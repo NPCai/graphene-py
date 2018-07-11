cf94c5650cb47452eb810366321b1b35b3b08109
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-11 13:54:08 -0400

    extracts finished

---
+++ wrapper.py
@@ -0,0 +1,43 @@
+import requests
+import re
+
+URL = "http://localhost:8080/relationExtraction/text"
+
+
+class GrapheneExtract(object):
+
+	def __init__(self, span):
+		payload = {
+		 "text": span,
+		 "doCoreference": "true", 
+		 "isolateSentences": "false", 
+		 "format": "FLAT_RESOLVED"
+		 }
+		r = requests.post(URL, json=payload)
+		self.json = {i['id']:i for i in r.json()['extractions']}
+		self.visited = {e:False for e in self.json} # (hash_id, bool is_used)
+
+	def linearize(self):
+		''' Depth-first search and append on the extracts '''
+		# TODO: reorder simple and linked contexts based on position in the origional sentence
+		self.strbuild = ""
+		for hashId in self.json:
+			self.visit(hashId)
+		self.strbuild =  self.strbuild.replace(".", "")
+		self.strbuild = re.sub(r"\s+", ' ', self.strbuild)
+		return self.strbuild.strip()
+
+	def visit(self, hashId):
+		if self.visited[hashId]:
+			return
+		self.visited[hashId] = True
+		self.strbuild += " ( " + self.json[hashId]['arg1'] + " <> " + self.json[hashId]['relation'] + " <> " + self.json[hashId]['arg2']
+		for simple in self.json[hashId]['simpleContexts']:
+			self.strbuild += " " + simple['classification'] + " " + simple['text']
+		for child in self.json[hashId]['linkedContexts']:
+			if not self.visited[child['targetID']]:
+				self.strbuild += " " + child['classification'] + " "
+				self.visit(child['targetID'])
+		self.strbuild += " ) "
+
+
